e7ac572bd0093f18975932403576222b1a645586
weekbin <yngweekbin@gmail.com>
2022-05-30 14:13:21 +0800

    feat: 修复了部分异常问题

--- clean.js
+++ clean.js
@@ -2,28 +2,41 @@ const fs = require("fs");
 const glob = require("glob");
 const path = require("path");
 
-// fs.readdir("./src", (err, stats) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(stats);
-//   stats.forEach((stat) => {
-//     const isDirectory = fs.lstatSync(`./src/${stat}`).isDirectory();
-//     if (isDirectory) {
-//       //
-//     }
-//   });
-// });
-
 class WebpackCleanUndependentFilesPlugin {
   constructor(options) {
     this.options = {
-      entry: ["./src"], // 清理文件入口
-      autoDelete: false, // 是否自动清除未被依赖的文件
-      outputLogs: true, // 是否输出日志
-      outputLogsPath: "./wcufp.json", // 日志输出目录
-      exclude: [], // 不清除的正则数组
+      /**
+       * 清理文件入口
+       * 适配多入口打包的情况，可以同时清理多个入口的非依赖文件
+       */
+      entry: ["./src"], // 
+      /**
+       * 是否自动清除未被依赖的文件
+       */
+      autoDelete: false,
+      /**
+       * 是否输出日志
+       */
+      outputLogs: true,
+      /**
+       * 日志输出目录
+       */
+      outputLogsPath: "./wcufp.json",
+      /**
+       * 正则数组，不被清除的未被依赖的文件
+       * 例如：utils 文件夹下的内容虽然当前不被引用，但是也不打算直接清除
+       */
+      exclude: [],
+      /**
+       * 正则数组，外部依赖，视为可清除的文件
+       * 例如：某个自己写的依赖采用模块的方式引入，但是现在打算用 script 的方式引入
+       */
+      externalDependencies: [],
+      /**
+       * 用户自定义处理函数，会传入 this.files2Delete，this 会帮定为类实例
+       * 存在 callback 将忽略 autoDelete 参数，不自动删除任何文件
+       */
+      callback: null,
       ...this.normalizeOptions(options),
     };
     this.dependencies = [];
@@ -37,6 +50,14 @@ class WebpackCleanUndependentFilesPlugin {
     if (options.entry) {
       options.entry = [...new Set(options.entry)];
     }
+    options.autoDelete = !!options.autoDelete;
+    options.outputLogs =
+      typeof options.outputLogs === "boolean" ? options.outputLogs : true;
+    if (options.exclude) {
+      options.exclude = options.exclude.filter((reg) => {
+        return reg instanceof RegExp || typeof reg === "string";
+      });
+    }
     return options;
   }
 
@@ -45,9 +66,16 @@ class WebpackCleanUndependentFilesPlugin {
     compiler.hooks.afterEmit.tap("cleanFiles", async (compilation) => {
       // webpack 生命周期钩子收集项目目录下自己写的依赖
       await this.gatherDependencies(compilation);
+      // 收集入口下的所有文件
       await this.gatherEntryFiles();
+      // 过滤出需要被删除的文件
       await this.gatherFiles2Delete();
-      if (this.options.autoDelete) {
+      // 如果存在 callback，调用 callback
+      if (this.options.callback) {
+        this.options.callback.call(this, this.files2Delete);
+      }
+      // 自动删除未被依赖的文件
+      if (this.options.autoDelete && !this.options.callback) {
         await this.cleanFiles();
         for (const entry of this.options.entry) {
           await this.cleanEmptyDirectory(entry);
@@ -59,7 +87,11 @@ class WebpackCleanUndependentFilesPlugin {
   // 收集 webpack 监测到的依赖
   async gatherDependencies(compilation) {
     this.dependencies = Array.from(compilation.fileDependencies).filter(
-      (path) => !path.includes("node_modules")
+      (path) =>
+        !path.includes("node_modules") &&
+        !this.options.externalDependencies.some((dependencyRegExp) => {
+          return !!path.match(dependencyRegExp);
+        })
     );
   }
 
@@ -71,7 +103,6 @@ class WebpackCleanUndependentFilesPlugin {
         new Promise((resolve, reject) => {
           glob(`${et}/**/*`, { nodir: true }, (err, files) => {
             if (err) {
-              console.log(err);
               reject(err);
             } else {
               resolve(
@@ -85,7 +116,9 @@ class WebpackCleanUndependentFilesPlugin {
       );
     });
     const allEntryFilesList = await Promise.all(promiseList);
-    this.allEntryFiles.push(...allEntryFilesList.flat());
+    this.allEntryFiles.push(
+      ...allEntryFilesList.filter((fileList) => Array.isArray(fileList)).flat()
+    );
   }
 
   // 过滤出需要被删除的文件
@@ -120,6 +153,7 @@ class WebpackCleanUndependentFilesPlugin {
     try {
       files = fs.readdirSync(filePath);
     } catch (error) {
+      // filePath is not a directory, ignore the error
       return;
     }
     if (files.length === 0) {
@@ -130,7 +164,6 @@ class WebpackCleanUndependentFilesPlugin {
         const nextFilePath = `${filePath}/${file}`;
         this.cleanEmptyDirectory(nextFilePath);
       });
-      //删除母文件夹下的所有字空文件夹后，将母文件夹也删除
       if (fs.readdirSync(filePath).length === 0) {
         fs.rmdirSync(filePath);
         console.log(`成功删除空文件夹： ${filePath}`);
